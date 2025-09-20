4eaefc86385f65d679681ee8316857842d5b662f
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2025-09-20 11:50:49 +0530

    newly
    assignment

---
+++ Task_1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter First Number: "))
+b=int(input("Enter Second Number: "))
+
+print("Your Addition is",a+b)
+print("Your Subtraction is",a-b)
+print("Your Multiplication is",a*b)
+print("Your Division is",a/b)
---
+++ Task_2.py
@@ -0,0 +1,4 @@
+a=str(input("Enter the first Name: "))
+b=str(input("Enter the last Name: "))
+
+print("Hello "+a+" "+b+"! Welcome to the Python program.")
